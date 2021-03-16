3284ad3d4808c9c1bace97102a01141faf711479
Rootdrigo <errecastillo@gmail.com>
2021-03-16 14:34:58 -0400

    xpath teste

--- spider.py
+++ spider.py
@@ -7,6 +7,24 @@ import networkx as nx
 
 
 
+
+#get spath of a soup element
+def xpath_soup(element):
+    components = []
+    child = element if element.name else element.parent
+    for parent in child.parents:  # type: bs4.element.Tag
+        siblings = parent.find_all(child.name, recursive=False)
+        components.append(
+            child.name if 1 == len(siblings) else '%s[%d]' % (
+                child.name,
+                next(i for i, s in enumerate(siblings, 1) if s is child)
+                )
+            )
+        child = parent
+    components.reverse()
+    return '/%s' % '/'.join(components)
+
+
 def get_hrefs( driver , main_url):
     try:
         driver.get(main_url)
